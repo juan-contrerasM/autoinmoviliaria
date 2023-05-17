ee37869c3d3a6fcd5dd2d3b165d2d96714dd38de
juan contreras <juanj.contrerasm@uqvirtual.edu.co>
2023-05-16 19:57:57 -0500

    first commit

---
+++ src/domain/person/Person.java
@@ -0,0 +1,5 @@
+package domain.person;
+
+public abstract class Person {
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,5 @@
+module automoviliaria {
+	requires javafx.controls;
+	
+	opens ui.start to javafx.graphics, javafx.fxml;
+}
